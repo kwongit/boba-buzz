9845717eaccb6e9f7ba8f84eab949bb23f1ce827
kevin <kevinwon30@gmail.com>
2023-10-30 13:46:51 -0700

    finished setting up featuredItemsReducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import session from "./session";
 import businessesReducer from "./businesses";
+import featuredItemsReducer from "./featuredItems";
 import reviewsReducer from "./reviews";
 
 const rootReducer = combineReducers({
   session,
   businesses: businessesReducer,
+  featuredItems: featuredItemsReducer,
   reviews: reviewsReducer,
 });
 
