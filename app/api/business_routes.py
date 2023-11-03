ae315291c45980ef113f4ac51791d5f74a1f5758
kevin <kevinwon30@gmail.com>
2023-11-03 11:29:34 -0700

    clean up

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from flask_login import current_user, login_required
 from app.models import Business, FeaturedItem, Review
 from app.models.db import db
